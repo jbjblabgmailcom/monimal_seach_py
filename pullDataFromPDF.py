b86711be8f834c903ee9b660bd6e7333ece16c6c
jbjblabgmailcom <131279867+jbjblabgmailcom@users.noreply.github.com>
2024-12-20 11:00:37 +0100

    Update pullDataFromPDF.py

--- pullDataFromPDF.py
+++ pullDataFromPDF.py
@@ -1,17 +1,27 @@
-﻿import os
+import os
 import re
 import fitz  # PyMuPDF
 import pandas as pd
 from collections import defaultdict
 from funkcje import dict_to_excel, extract_numbers_from_raports, pull_nominals_from_excell, extract_column_values, is_number
-from funkcje import search_for_nominals_in_raports
+from search import search_for_nominals_in_raports
+from funkcje import create_file_list, combine_nested_dicts
 
 
 
 if __name__ == "__main__":
     
   
-    nominals_from_excell = pull_nominals_from_excell('/nominaly/')
-    dane_znalezione = search_for_nominals_in_raports(nominals_from_excell)
-    dict_to_excel(dane_znalezione)
+    nominals_from_excell_list = pull_nominals_from_excell('/nominaly/')
+    file_list_dict = create_file_list('raporty')
+    print(file_list_dict)
+    dane_znalezione = search_for_nominals_in_raports(nominals_from_excell_list, file_list_dict)
+    #print(dane_znalezione['nikon-altera'])
+    #print(dane_znalezione['mitutoyo-crysta-apex'])
+    # print(dane_znalezione['mitutoyo-reczna'])
+    #print(dane_znalezione['nikon-altera-rpt'])
+    #print(dane_znalezione)
+    new_dict = combine_nested_dicts(dane_znalezione)
+    #print(new_dict)
+    dict_to_excel(new_dict)
 
